372e1ef4e8912f2cfafc131bd620356013c6af06
Tadeo <tadeo@do-mix.de>
2020-10-20 21:09:05 +0200

    changed papi url

--- 2020-10-02-menumori-frontend/src/services/DataService.js
+++ 2020-10-02-menumori-frontend/src/services/DataService.js
@@ -1,12 +1,15 @@
 /////////////////////////////////////////////////////////////////////
 //
 //
+/////////////////////////////////////////////////////////////////////
 
 import axios from "axios";
 
 // DEFINE ALL THE CONNECTION VARIABLES:
 export const IAPIURL = "http://localhost:1337"; // internal API URL = Strapi url, connects directly to the database, all routes are at least jwt protected even for get requests.
-export const PAPIURL = "http://localhost:2000"; // public API URL = menumori-bot-api, routes can be accessed by anywhere
+export const PAPIURL = "http://localhost:1338"; // public API URL = menumori-bot-api / indexpapi, routes can be accessed by anywhere
+
+/////////////////////////////////////////////////////////////////////
 //
 //
 /////////////////////////////////////////////////////////////////////
