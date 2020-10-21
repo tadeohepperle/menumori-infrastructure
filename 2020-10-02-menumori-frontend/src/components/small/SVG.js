dca2858f9fbf40d4da1f1c64036b3884b23f92cb
Tadeo <tadeo@do-mix.de>
2020-10-21 12:54:16 +0200

    svgs

---
+++ 2020-10-02-menumori-frontend/src/components/small/SVG.js
@@ -0,0 +1,42 @@
+export function SVG_Tailwindlogo() {
+  return (
+    <svg
+      class="fill-current h-8 w-8 mr-2"
+      width="54"
+      height="54"
+      viewBox="0 0 54 54"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+    </svg>
+  );
+}
+export function SVG_Menu() {
+  return (
+    <svg
+      class="fill-current h-3 w-3"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Menu</title>
+      <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+    </svg>
+  );
+}
+
+export function SVG_Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="w-10 h-10 text-white p-2 bg-teal-500 rounded-full"
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+    </svg>
+  );
+}
